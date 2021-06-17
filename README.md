611ba21b39983aa410fbf9665cb3954692283fda
Charles Severance <csev@umich.edu>
2021-06-17 11:35:32 -0400

    Updates

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 
+Scanned June 2021 
+
+Epson WS-200
+
+Scanned to JPG at 400DPI
+OCR using Epson Scan to Word
+
 https://word2md.com/
 
 Unbold everything in the word doc before converting to Markdown
--- kr/clean.py
+++ kr/clean.py
@@ -74,7 +74,7 @@ def iscode(line) :
     return '    '+line
 
 # Note tables are vertical bars
-hand = open("chap03/chap03_orig.md")
+hand = open("chap04/chap04_orig.md")
 # wordsplit = u'\u00AD'.encode('utf-8')
 wordsplit = u'\u00AD'
 chaptext = ""
