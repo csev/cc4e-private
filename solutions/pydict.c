2e644d13a63bdd050196c38cf9a7dbd76b7cf360
Charles Severance <csev@umich.edu>
2023-03-04 13:24:15 -0500

    Add swap

--- solutions/pydict.c
+++ solutions/pydict.c
@@ -128,6 +128,57 @@ int pydict_index(struct pydict* self, char *str)
     return -1;
 }
 
+void private_pydict_swap(struct pydict* self, struct dnode *cur){
+
+    struct dnode *prev, *next, *rest;
+
+    // Save the cur values
+    next = cur->next;        // Will always exist
+    prev = cur->prev;        // May be null if at the front
+    rest = cur->next->next;  // May be null if next is at the end
+
+    if ( prev != NULL ) {
+        prev->next = next;
+    } else {
+        self->head = next;
+    }
+
+    cur->next = rest;
+    cur->prev = next;
+
+    next->next = cur;
+    next->prev = prev;
+
+    if ( rest != NULL ) {
+        rest->prev = cur;
+    } else {
+        self->tail = cur;
+    }
+
+}
+
+// Bubble sort by key - Order N**2
+struct pydict* pydict_ksort(struct pydict* self) {
+
+    struct dnode *cur;
+    int i;
+
+    if ( self->head == NULL ) return self;
+
+    for (i=0; i<=self->count; i++) {
+        for(cur = self->head; cur != NULL ; cur = cur->next ) {
+            if ( cur->next == NULL ) continue;  // Last item in the list
+            // In order already
+            if ( strcmp(cur->key, cur->next->key) <= 0 ) continue;
+
+            // Lets swap
+            // printf("Flipping %s %s\n", cur->value, cur->next->value);
+            private_pydict_swap(self, cur);
+        }
+    }
+    return self;
+}
+
 // Bubble sort by value - Order N**2
 struct pydict* pydict_vsort(struct pydict* self) {
 
@@ -142,81 +193,13 @@ struct pydict* pydict_vsort(struct pydict* self) {
             // In order already
             if ( strcmp(cur->value, cur->next->value) <= 0 ) continue;
 
-            // Lets swap
             // printf("Flipping %s %s\n", cur->value, cur->next->value);
-
-            // Save the cur values
-            next = cur->next;        // Will always exist
-            prev = cur->prev;        // May be null if at the front
-            rest = cur->next->next;  // May be null if next is at the end
-
-            if ( prev != NULL ) {
-                prev->next = next;
-            } else {
-                self->head = next;
-            }
-
-            cur->next = rest;
-            cur->prev = next;
-
-            next->next = cur;
-            next->prev = prev;
-
-            if ( rest != NULL ) {
-                rest->prev = cur;
-            } else {
-                self->tail = cur;
-            }
-
+            private_pydict_swap(self, cur);
         }
     }
     return self;
 }
 
-// Bubble sort by key - Order N**2
-struct pydict* pydict_ksort(struct pydict* self) {
-
-    struct dnode *prev, *cur, *next, *rest;
-    int i;
-
-    if ( self->head == NULL ) return self;
-
-    for (i=0; i<=self->count; i++) {
-        for(cur = self->head; cur != NULL ; cur = cur->next ) {
-            if ( cur->next == NULL ) continue;  // Last item in the list
-            // In order already
-            if ( strcmp(cur->key, cur->next->key) <= 0 ) continue;
-
-            // Lets swap
-            // printf("Flipping %s %s\n", cur->value, cur->next->value);
-
-            // Save the cur values
-            next = cur->next;        // Will always exist
-            prev = cur->prev;        // May be null if at the front
-            rest = cur->next->next;  // May be null if next is at the end
-
-            if ( prev != NULL ) {
-                prev->next = next;
-            } else {
-                self->head = next;
-            }
-
-            cur->next = rest;
-            cur->prev = next;
-
-            next->next = cur;
-            next->prev = prev;
-
-            if ( rest != NULL ) {
-                rest->prev = cur;
-            } else {
-                self->tail = cur;
-            }
-
-        }
-    }
-    return self;
-}
 
 int main(void)
 {
