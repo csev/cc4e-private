d979eb22d01ae2b59184f45608f61039ea84ed6a
Charles Severance <csev@umich.edu>
2023-04-10 08:36:28 -0400

    Refactor into two files

--- solutions/map_tree_test.c
+++ solutions/map_tree_test.c
@@ -313,3 +313,38 @@ struct Map * Map_new() {
     p->del = &__Map_del;
     return p;
 }
+
+/**
+ * The main program to test and exercise the Map 
+ * and MapEntry classes.
+ */
+int main(void)
+{
+    struct Map * map = Map_new();
+    struct MapEntry *cur;
+    struct MapIter *iter;
+
+    map->put(map, "r", 8);
+    map->put(map, "y", 2);
+    map->put(map, "a", 4);
+    map->put(map, "b", 3);
+    map->put(map, "r", 1);
+    map->dump(map);
+
+    printf("r=%d\n", map->get(map, "r", 42));
+    printf("x=%d\n", map->get(map, "x", 42));
+
+    printf("\nIterate\n");
+    iter = map->iter(map);
+    while(1) {
+        cur = iter->next(iter);
+        if ( cur == NULL ) break;
+        printf(" %s=%d\n", cur->key, cur->value);
+    }
+    iter->del(iter);
+
+    map->del(map);
+}
+
+// rm -f a.out ; gcc map_tree.c; a.out ; rm -f a.out
+
